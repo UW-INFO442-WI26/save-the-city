d3112a6600246c84dab4699d230bd90d8dbe89fc
Isaac Gurney <isaacg6@uw.edu>
2026-01-28 10:03:18 -0800

    sets up firebase sdk

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUtXlv_JQgrxcrNFCYLK2FYSFyqb7ANc4",
+  authDomain: "save-the-city-906f2.firebaseapp.com",
+  projectId: "save-the-city-906f2",
+  storageBucket: "save-the-city-906f2.firebasestorage.app",
+  messagingSenderId: "754921736073",
+  appId: "1:754921736073:web:842d06494ac8305a850358"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
