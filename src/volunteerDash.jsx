ece739ad63fe29f94c4797b0fa51695d0734eab1
Isaac Gurney <isaacg6@uw.edu>
2026-02-17 09:01:03 -0800

    fix typo on volunteer page for clarity

--- src/volunteerDash.jsx
+++ src/volunteerDash.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function VolunteerDash() {
   return (
     <div className="container mt-5">
-      <h1 className="mb-4">Host Dashboard</h1>
+      <h1 className="mb-4">Volunteer Dashboard</h1>
       <p>Welcome to the Volunteer Dashboard!</p>
       {/* Add more dashboard features here */}
     </div>
