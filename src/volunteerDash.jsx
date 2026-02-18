bd760beb96aa8e114cb5288ebe7f037c21071f4b
Isaac Gurney <isaacg6@uw.edu>
2026-02-17 17:46:35 -0800

    install + import leaflet - get map working on volunteer dash centered on seattle metro area

--- src/volunteerDash.jsx
+++ src/volunteerDash.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { MapContainer, TileLayer, useMapEvents, Marker } from 'react-leaflet'
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+})
 
 export default function VolunteerDash() {
   return (
-    <div className="container mt-5">
-      <h1 className="mb-4">Volunteer Dashboard</h1>
-      <p>Welcome to the Volunteer Dashboard!</p>
-      {/* Add more dashboard features here */}
-    </div>
+    <MapContainer
+      center={[47.6206, -122.3493]} // Seattle Center coordinates
+      zoom={13}
+      className="map-container">
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+    </MapContainer>
   );
 }
