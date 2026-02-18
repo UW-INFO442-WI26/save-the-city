49f4a1b1a90844d15c1f7e6fef26c19e653a2289
Isaac Gurney <isaacg6@uw.edu>
2026-02-17 18:27:30 -0800

    refactor map to be in it's own component

---
+++ src/mapWindow.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MapContainer, TileLayer, useMapEvents, Marker } from 'react-leaflet'
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+})
+
+export default function MapWindow() {
+  return (
+    <MapContainer
+      center={[47.6206, -122.3493]} // Seattle Center coordinates
+      zoom={13}
+      className="map-container">
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+    </MapContainer>
+  );
+}
--- src/volunteerDash.jsx
+++ src/volunteerDash.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { MapContainer, TileLayer, useMapEvents, Marker } from 'react-leaflet'
-import L from 'leaflet'
-
-delete L.Icon.Default.prototype._getIconUrl
-L.Icon.Default.mergeOptions({
-  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
-  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
-})
+import MapWindow from './mapWindow';
 
 export default function VolunteerDash() {
   return (
-    <MapContainer
-      center={[47.6206, -122.3493]} // Seattle Center coordinates
-      zoom={13}
-      className="map-container">
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-    </MapContainer>
+    <div>
+      <MapWindow />
+    </div>
   );
 }
